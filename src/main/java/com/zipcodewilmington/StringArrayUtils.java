1fc2cbe4c8e30985b321141c4eebaf76ed79431c
mike <mike@zipcodes-mbp-5.lan>
2021-03-05 23:53:39 -0500

    completed

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -25,7 +27,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length-1];
     }
 
     /**
@@ -33,7 +35,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length-2];
     }
 
     /**
@@ -42,7 +44,11 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
+        for (int i = 0; i <= array.length-1; i++){
+            if (array[i].equals(value)){
+                return true;
+            }
+        }return false;
     }
 
     /**
@@ -50,7 +56,11 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] rev = new String[array.length];
+        for (int i = 0; i < array.length; i++){
+            rev[(array.length -1) - i] = array[i];
+        }
+        return rev;
     }
 
     /**
@@ -58,7 +68,11 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
+        for (int i = 0; i < array.length; i++){
+            if(array[i].equals(array[array.length-1 - i])){
+                return true;
+            }
+        }return false;
     }
 
     /**
@@ -66,7 +80,19 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+        boolean result = true;
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        String a = Arrays.toString(array).toLowerCase();
+        String[] alphaArray = alpha.split(""); //split string alpha into string array
+        for (int i = 0; i < alpha.length() - 1; i++) {
+            if (!a.contains(alphaArray[i])) {
+                result = false;
+
+            } else
+            {
+                result = true;
+            }
+        } return result;
     }
 
     /**
@@ -75,7 +101,13 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        int occurrences = 0;
+        for(int i = 0; i< array.length; i++){
+            if (array[i]==value){
+                occurrences += 1;
+            }
+        }
+        return occurrences;
     }
 
     /**
@@ -84,7 +116,14 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        String remainder = "";
+        for (int i = 0; i < array.length; i++){
+            if (!array[i].equals(valueToRemove)){
+                remainder += array[i] + " ";
+            }
+        }
+        String[] leftover = remainder.split(" ");
+        return leftover;
     }
 
     /**
@@ -92,15 +131,78 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
+        int count = 0;
+        String[] a = array;
+
+        for (int i = 0; i < array.length-1; i++){
+            if(a[i] == a[i+1]){
+                count++;}
+
+        }
+        System.out.println(count);
+        String[] dupChars = new String[a.length-count];
+        // Temp string to store duplicate characters
+        String tempString = a[0];
+        // Current position for dupChars array
+        int position = 0;
+        for (int i = 0; i < a.length-1; i++) {
+            if (a[i] != a[i+1]) {
+                dupChars[position] = tempString;
+                tempString = a[i+1];
+                position++;
+            } else {
+                tempString = a[i];
+            }
+        }
+        dupChars[position] = a[a.length-1];
+        for (int i = 0; i < dupChars.length; i++) {
+            System.out.println(dupChars[i]);
+        }
+        return dupChars;
+
+
     }
+
+
 
     /**
      * @param array array of chars
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+        int count = 1;
+        String[] a = array;
+
+        for (int i = 0; i < array.length-1; i++){
+            if(a[i] != a[i+1]) count++;
+        }
+
+        String[] dupChars = new String[count];
+
+        // Temp string to store duplicate characters
+        String tempString = a[0];
+        // Current position for dupChars array
+        int position = 0;
+        for (int i = 0; i < a.length; i++) {
+
+            if(i == a.length-1) dupChars[position] = tempString;
+            else {
+                if (a[i] != a[i + 1]) {
+                    dupChars[position] = tempString;
+                    tempString = a[i + 1];
+                    position++;
+                } else {
+                    tempString += a[i + 1];
+                }
+            }
+        }
+
+//        for (int i = 0; i < dupChars.length; i++){
+//            System.out.println(dupChars[i]);
+//        }
+        // for checking every array
+        return dupChars;
+
     }
 
 
